62cec9a049e655cb46f516f2598c765596872313
uimoow <67147254+uimoow@users.noreply.github.com>
2025-03-16 09:00:58 +0000

    Updated Flask app with Dockerfile and Docker Compose for Flask-MySQL setup

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
+# MySQL configurations
+app.config['MYSQL_HOST'] = os.getenv('DB_HOST')
+app.config['MYSQL_USER'] = os.getenv('DB_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('DB_NAME')
+
+mysql = MySQL(app)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Hello, World with MySQL!'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
